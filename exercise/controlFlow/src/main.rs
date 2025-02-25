af3009b152c1ec0af2f737c89ddf62cb28fff555
dimka90 <dimkayilrit@gmail.com>
2025-02-25 21:58:26 +0100

    feat: added a function create a fibonacci sequeunce

--- exercise/controlFlow/src/main.rs
+++ exercise/controlFlow/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("Hello, world!");
     println!("{}", fahrenhiet_to_celsius(celsius_to_fahrenhiet(37.7)));
+
+    println!("{}",fib(4));
 }
 
 // C= 9/5(F−32)
@@ -15,3 +17,16 @@ fn celsius_to_fahrenhiet(cel_measurement: f64) -> f64 {
     let celsius = ((cel_measurement * 9.0) / 5.0) + 32.0;
     return celsius;
 }
+
+
+fn fib(num: i32) -> i32 {
+    if num <= 0 {
+        return 0;
+    } else if num == 1 {
+        return 1;
+    } else {
+        return fib(num - 1) + fib(num - 2);
+    }
+}
+
+
