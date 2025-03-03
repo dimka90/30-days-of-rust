070396801d5566da6a120dba5fdf9e6ffba46941
dimka90 <dimkayilrit@gmail.com>
2025-03-03 20:32:09 +0100

    feat:added comment to functions

--- exercise/storage/src/main.rs
+++ exercise/storage/src/main.rs
@@ -1,14 +1,30 @@
 
-use std::fs::File;
-use std::io::{ Stdin, Write};
+use std::fs::{File, OpenOptions};
+use std::io::{ Read, Stdin, Write};
 use std::io;
 struct User{
     name: String,
     age:u64,
 }
+impl User {
+
+    fn get_user_details(&self) ->u64{
+        return self.age
+    }
+    
+}
 fn main() {
 collect_user_data();
+read_users_from_file();
 
+let user = User{
+    name: String::from("Jesus"),
+    age: 30,   
+};
+
+
+println!("Age {}",user.get_user_details());
+println!("{}", User::get_user_details(&user));
 }
 
 
@@ -21,6 +37,7 @@ fn  collect_user_data(){
     let _read_line = standard_input.read_line(&mut username).expect("File to read user Name");
     println!("Age: ");
     let _read_line = standard_input.read_line(&mut userage).expect("File to read user age");
+    // Remove newline character from the user input from the keyboard
     let age: u64= userage.trim().parse().expect("Fail to pass it");
     create_user(&username, age);
 
@@ -35,19 +52,44 @@ fn  create_user( username: &str, age : u64){
 }
 
 fn conver_to_string(user: &User){
-
-    let formated_string = format!("Name: {}, Age {}", user.name, user.age);
-    let filename = format!("{}.txt", user.name.clone());
-    let _data = match store_user_to_file(&formated_string, &filename){
+    // This function convert user struct into a  string format for storage in file
+    let formated_string = format!("Name: {}, Age {}\n", user.name, user.age);
+    // let filename = format!("{}.txt", user.name.clone());
+    let _data = match store_user_to_file(&formated_string){
         Ok(_) => println!("User sucessfully created"),
         Err(err) => println!("Error: An Error Occured {}", err),
     };
 }
 
-fn store_user_to_file(user_details: &str, filename: &str) -> std::io::Result<()>{
+fn store_user_to_file(user_details: &str) -> std::io::Result<()>{
+    // Using file OpenOptions to either create a file if none-exist,
+    // Open and append to a file
+    let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("blob.txt")
+            .expect("Error: File Error");
 
-    let mut file: File = File::create(filename)?;
     file.write_all(user_details.as_bytes())?;
     Ok(())
 
 }
+
+fn  read_users_from_file(){
+
+    // open file
+    let mut  file = File::open("blob.txt").unwrap();
+    let mut user_datails = String::new();
+    // read user content from the file line by line
+    file.read_to_string(&mut user_datails).expect("Error");
+
+    println!("================");
+    println!("User Details");
+
+    for lines in user_datails.lines(){
+
+        println!("{}", lines)
+    }
+
+}
+
