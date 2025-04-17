d82275ab4c8b6cbcc34c90823dd56771f11963d8
dimka90 <dimkayilrit@gmail.com>
2025-04-17 04:32:28 +0100

    Added a function that print the address of a link_list stored on the heap

--- Box/smart_box/src/main.rs
+++ Box/smart_box/src/main.rs
@@ -3,10 +3,14 @@ use types::types::List;
 fn main() {
 
     let linked_list = List::Cons(1, 
-                        Box::new(List::Cons(2, 
-                            Box::new(List::Cons(3,
-                                 Box::new(List::Nil))))));
+                        Box::new(List::Cons(
+                            2, Box::new(List::Cons(
+                                3,
+                                 Box::new(
+                                    List::Nil))))));
 linked_list.traverse();
+linked_list.reverse();
+linked_list.get_pointer_for_stored();
 }
 
 
--- Box/smart_box/src/types.rs
+++ Box/smart_box/src/types.rs
@@ -1,5 +1,5 @@
 pub mod types {
-
+#[derive(Debug)]
 pub enum List{
 Cons(i32, Box<List>),
 Nil
@@ -21,6 +21,36 @@ pub fn traverse(&self) {
         current = next;
     }
 }
+pub fn reverse(&self) {
 
+    let mut  current = self;
+
+    let count = 1;
+    let mut vector: Vec<i32> = Vec::new();
+    while let Self::Cons(value,next ) = current{
+        vector.push(*value); 
+        current = next;
+    }
+
+    println!("Reverse other");
+    while let Some(num) = vector.pop(){
+      
+        print!("{ }", num);
+    }
+
+}
+
+pub fn get_pointer_for_stored(&self) {
+    let mut  current = self;
+    let count = 1;
+
+    while  let Self::Cons(value, next)  = current{
+
+        println!("Pointer address {:p}", &*next);
+        // assert_eq!(next, *next);
+        current = next;
+        
+    }
+}
 }
 
