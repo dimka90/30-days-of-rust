847c4421ddbf8a5d429db5ab67cae35297c62cce
dimka90 <dimkayilrit@gmail.com>
2025-04-19 21:02:23 +0100

    implemented an Ilterator trait for a struct

--- Traits/traits/src/main.rs
+++ Traits/traits/src/main.rs
@@ -1,12 +1,29 @@
 mod  types;
-
+use std::{fmt::Display, vec};
 use types::Types::Book;
 fn main() {
 
-    let book1 = Book{
-        title:String::from( "Things fall apart"),
+    let mut book1 = Book{
+        books: vec![String::from("Things fall apart"), 
+        String::from("Invinsible"),
+        String::from("Think and grow rich")],
         pages: 300,
+        count: 0,
+        len: 3
     };
 
     println!("{}", book1);
+    let word = "Hello";
+    print_twice(&word);
+
+    while let Some(book)= book1.next() {
+        println!("{book}")
+}
+}
+
+fn print_twice<T: Display>(data: &T){
+
+    for i in 1..=2{
+        println!("{}", data)
+    }
 }
--- Traits/traits/src/types.rs
+++ Traits/traits/src/types.rs
@@ -1,16 +1,35 @@
-use std::fmt;
+use std::fmt::{self, Display};
+
+use Types::Book;
 
 pub mod Types{
 
+    
     pub struct Book{
-       pub title: String,
-        pub pages: u16
+       pub books: Vec<String>,
+        pub pages: u16,
+        pub count: i16,
+        pub len: usize
     }
 }
 
 impl fmt::Display for Types::Book{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         
-       write!(f, "{}, {}", self.title, self.pages)
+       write!(f, "{:?}, {}", self.books, self.pages)
+    }
+}
+
+impl  Iterator for Book {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count <  self.len  as i16{
+        let result = self.books[self.count as usize].clone();
+        self.count+=1;
+        return  Some(result);
+        }
+        None
+
     }
 }
