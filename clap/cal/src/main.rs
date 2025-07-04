cc48d72aa50cb00b20560785a00ce50a823f9b83
dimka90 <dimkayilrit@gmail.com>
2025-07-04 06:11:19 +0100

    feat: added functionality for adding two numbers

--- clap/cal/src/main.rs
+++ clap/cal/src/main.rs
@@ -1,3 +1,6 @@
+use core::num;
+use std::{process::CommandArgs, result};
+
 use clap::{Arg, Command};
 
 fn main() {
@@ -10,9 +13,20 @@ fn main() {
                                         Arg::new("Calculator")
                                         .long("Calculator")
                                         .value_name("first Number")
+                                        .num_args(2)
                                         .value_parser(clap::value_parser!(i32))
                                     )
                                 .get_matches();
 
-    println!("My first app {:?}", new_command_line);
+    if let Some(values) =  new_command_line.get_many::<i32>("Calculator"){
+
+        let numbers: Vec<i32> = values.copied().collect();
+        let first_number = numbers[0];
+        let second_number = numbers[1];
+        let result = first_number + second_number;
+        println!("Result {:?}", result);
+
+
+    }
+    
 }
