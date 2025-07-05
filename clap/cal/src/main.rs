2b05ffbb90854938d4bd38c7d9c14d065b2fd0d9
dimka90 <dimkayilrit@gmail.com>
2025-07-05 15:15:58 +0100

    modify the code to a more clean strcuture

--- clap/cal/src/main.rs
+++ clap/cal/src/main.rs
@@ -1,32 +1,43 @@
+use std::process;
+
 use clap::{Arg, Command};
 
 fn main() {
-
-    let new_command_line = Command::new("Calculator")
-                                    .author("Dimka")
-                                    .about("A Calculator cli that perform simple multiplication")
-                                    .version("v.0.1")
-                                    .subcommand(
-                                        Command::new("add")
-                                        .about("A function that adds to two numbers")
-                                    .arg(
-                                        Arg::new("numbers")
-                                        .long("cal")
-                                        .value_name("first Number")
-                                        .num_args(2)
-                                        .required(true)
-                                        .value_parser(clap::value_parser!(i32))
-                                    ),
-                                )
-                                .get_matches();
-
-    if let Some(values) =  new_command_line.get_many::<i32>("numbers"){
-
-        let numbers: Vec<i32> = values.copied().collect();
-        let result = numbers[1]+ numbers[0];
-        println!("Result {:?}", result);
-
-
+    let matches = Command::new("Calculator")
+        .author("Dimka")
+        .about("A Calculator CLI that performs simple arithmetic operations")
+        .version("v0.1.0")
+        .subcommand(
+            Command::new("add")
+                .about("Add two numbers")
+                .arg(
+                    Arg::new("first_number")
+                        .value_name("FIRST")
+                        .help("First number to add")
+                        .required(true)
+                        .value_parser(clap::value_parser!(i32)),
+                )
+                .arg(
+                    Arg::new("second_number")
+                        .value_name("SECOND")
+                        .help("Second number to add")
+                        .required(true)
+                        .value_parser(clap::value_parser!(i32)),
+                ),
+        )
+        .get_matches();
+    match matches.subcommand() {
+        Some(("add", add_matches)) => {
+            if let (Some(first), Some(second)) = (
+                add_matches.get_one::<i32>("first_number"),
+                add_matches.get_one::<i32>("second_number"),
+            ) {
+                println!("Result: {:?}", first + second);
+            } else {
+                eprint!("Both input are required");
+                process::exit(1);
+            }
+        }
+        _ => println!("Invalid input"),
     }
-    
 }
