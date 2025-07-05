ed6010fb8feabdc06b83fe0d1c2584cdbdb9655d
dimka90 <dimkayilrit@gmail.com>
2025-07-05 15:29:17 +0100

    feat: added a multiplication subcommand

--- clap/cal/src/main.rs
+++ clap/cal/src/main.rs
@@ -23,8 +23,24 @@ fn main() {
                         .help("Second number to add")
                         .required(true)
                         .value_parser(clap::value_parser!(i32)),
-                ),
-        )
+                )
+            )
+            .subcommand(
+                Command::new("mul")
+                .about("Multiple two numbers")
+                .arg(
+                    Arg::new("first_number")
+                    .value_name("FIRST NUMBER")
+                    .required(true)
+                    .value_parser(clap::value_parser!(i32))
+                )
+                .arg(
+                    Arg::new("second_number")
+                    .value_name("Second Number ")
+                    .required(true)
+                    .value_parser(clap::value_parser!(i32))
+                )
+            )
         .get_matches();
     match matches.subcommand() {
         Some(("add", add_matches)) => {
@@ -38,6 +54,23 @@ fn main() {
                 process::exit(1);
             }
         }
+
+        Some(("mul", mul_matches)) =>{
+            if let (Some(first), Some(second)) = (
+                mul_matches.get_one::<i32>("first_number"),
+                mul_matches.get_one::<i32>("second_number")
+            ){
+
+                let sum = first * second;
+
+                println!("Result: {:?}", sum);
+            }
+
+            else{
+                eprint!("Invalid input for multiplication");
+                process::exit(1);
+            }
+        },
         _ => println!("Invalid input"),
     }
 }
