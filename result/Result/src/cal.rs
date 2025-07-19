6cc650341bbe11f2e486cc681436277fa82a7029
dimka90 <dimkayilrit@gmail.com>
2025-07-19 04:27:52 +0100

    test: added tests for the function parse_age

--- result/Result/src/cal.rs
+++ result/Result/src/cal.rs
@@ -8,3 +8,21 @@ pub fn parse_age(age: &str) -> Result<u8, String>
 Err("Invalid number".into())
     }
 }
+
+#[cfg(test)]
+mod tests{
+    use core::num;
+
+    use super::*;
+
+    #[test]
+    fn test_valid_age() {
+        let num = parse_age("45").ok().unwrap();
+
+        assert_eq!(num, 45);
+    }
+    #[test]
+    fn test_invalid_age(){
+        assert_eq!(parse_age("uii"), Err("Invalid number".into()));
+    }
+}
