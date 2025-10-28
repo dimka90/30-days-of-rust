8dcd072fa3a4bf058309c0a7e27f6cbb4017e75e
dimka90 <dimkayilrit@gmail.com>
2025-10-28 22:50:27 +0100

    max: implemeted a max function that returns the larger function

--- quicksetup/function/src/main.rs
+++ quicksetup/function/src/main.rs
@@ -1,3 +1,10 @@
+fn max(a: i32, b: i32) -> i32{
+    if a > b{
+        return  a;
+    }
+    return  b;
+}
+
 // function with numbers
 fn add_number(first_number: f64, second_number: f64) -> f64 {
     return first_number + second_number;
@@ -11,5 +18,6 @@ fn print_count(number: i32) {
 fn main() {
     let sum = add_number(4.1, 6.3);
     print_count(10);
+    println!("Max : {}", max(10, 100));
     print!("{}", sum);
 }
