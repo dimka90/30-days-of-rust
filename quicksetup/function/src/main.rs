6a4b1fce35fa079f6e096c384ef064c85c61f771
dimka90 <dimkayilrit@gmail.com>
2025-10-28 22:26:46 +0100

    sum: implemented addition for two numbers

---
+++ quicksetup/function/src/main.rs
@@ -0,0 +1,11 @@
+
+
+fn add_number(first_number: f64, second_number: f64)  -> f64{
+
+    return first_number + second_number;
+}
+fn main() {
+    let sum = add_number(4.1, 6.3);
+    print!("{}", sum);
+    println!("Hello, world!");
+}
