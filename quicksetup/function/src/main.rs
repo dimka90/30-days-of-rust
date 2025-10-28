b0be432d5bf73cca2f977768ac7798bff37db81f
dimka90 <dimkayilrit@gmail.com>
2025-10-28 22:53:22 +0100

    max: implemeted a max function that returns the larger function

--- quicksetup/function/src/main.rs
+++ quicksetup/function/src/main.rs
@@ -1,15 +1,12 @@
-fn max(a: i32, b: i32) -> i32{
-    if a > b{
-        return  a;
+fn max(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a;
     }
-    return  b;
+    return b;
 }
-
-// function with numbers
 fn add_number(first_number: f64, second_number: f64) -> f64 {
     return first_number + second_number;
 }
-// functions with looops
 fn print_count(number: i32) {
     for i in 1..=number {
         println!("{i}");
