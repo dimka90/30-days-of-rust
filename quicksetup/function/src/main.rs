0cb091312ac1830f2acc0cc14b286c7bae437516
dimka90 <dimkayilrit@gmail.com>
2025-10-28 22:44:06 +0100

    fix inconsistant spaces

--- quicksetup/function/src/main.rs
+++ quicksetup/function/src/main.rs
@@ -1,24 +1,15 @@
-use core::num;
-
-
-
-
 // function with numbers
-fn add_number(first_number: f64, second_number: f64)  -> f64{
-
+fn add_number(first_number: f64, second_number: f64) -> f64 {
     return first_number + second_number;
 }
 // functions with looops
-fn print_count( number: i32) {
-    for i in 1..=number{
-        println!(" {i}");
+fn print_count(number: i32) {
+    for i in 1..=number {
+        println!("{i}");
     }
 }
-
-
-
 fn main() {
     let sum = add_number(4.1, 6.3);
+    print_count(10);
     print!("{}", sum);
-    println!("Hello, world!");
 }
