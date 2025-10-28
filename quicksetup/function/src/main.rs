8f4dd417b142a5d33708f02062a629218f0ae6fd
dimka90 <dimkayilrit@gmail.com>
2025-10-28 22:35:40 +0100

    loop:implemented loops in a functions

--- quicksetup/function/src/main.rs
+++ quicksetup/function/src/main.rs
@@ -1,9 +1,22 @@
+use core::num;
 
 
+
+
+// function with numbers
 fn add_number(first_number: f64, second_number: f64)  -> f64{
 
     return first_number + second_number;
 }
+// functions with looops
+fn print_count( number: i32) {
+    for i in 1..=number{
+        println!(" {i}");
+    }
+}
+
+
+
 fn main() {
     let sum = add_number(4.1, 6.3);
     print!("{}", sum);
