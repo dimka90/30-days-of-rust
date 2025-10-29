241242e58a151c63ab0fb9ef894691635b8aba56
dimka90 <dimkayilrit@gmail.com>
2025-10-29 21:48:46 +0100

    db-confing: created  dbconfig

--- quicksetup/ownership/src/database/database.rs
+++ quicksetup/ownership/src/database/database.rs
@@ -5,3 +5,11 @@ pub struct DatabaseConfig {
     password: String,
     max_connections: u32,
 }
+
+pub struct DatabaseConfigBuilder{
+    host: Option<String>,
+    port: Option<u16>,
+    username:Option<String>,
+    password: Option<String>,
+    max_connections: Option<u32>
+}
