de45ec3dc73e2c87e8588a74dde918cb42b5848a
dimka90 <dimkayilrit@gmail.com>
2025-10-29 22:00:53 +0100

    created and returned an instance of the Databasebuilder

--- quicksetup/ownership/src/database/database.rs
+++ quicksetup/ownership/src/database/database.rs
@@ -6,10 +6,27 @@ pub struct DatabaseConfig {
     max_connections: u32,
 }
 
-pub struct DatabaseConfigBuilder{
+#[derive(Debug)]
+pub struct DatabaseConfigBuilder {
     host: Option<String>,
     port: Option<u16>,
-    username:Option<String>,
+    username: Option<String>,
     password: Option<String>,
-    max_connections: Option<u32>
+    max_connections: Option<u32>,
+}
+
+impl DatabaseConfigBuilder {
+    pub fn new() -> Self {
+        Self {
+            host: None,
+            port: None,
+            username: None,
+            password: None,
+            max_connections: None,
+        }
+    }
+    pub fn port(mut self, host: String) -> Self {
+        self.host =  Some(host);
+        self
+    }
 }
--- quicksetup/ownership/src/main.rs
+++ quicksetup/ownership/src/main.rs
@@ -1,6 +1,7 @@
 mod database;
 
-use crate::database::database::DatabaseConfig;
+use crate::database::database::{DatabaseConfig, DatabaseConfigBuilder};
 fn main() {
-    
+    let postgress = DatabaseConfigBuilder::new();
+    println!("{:?}", postgress);
 }
