40372892bf705b68df88ab94fe28310c07192e9c
dimka90 <dimkayilrit@gmail.com>
2025-11-06 07:46:13 +0100

    implemented filtering for adult age

--- quicksetup/builder-pattern/iteralator/src/main.rs
+++ quicksetup/builder-pattern/iteralator/src/main.rs
@@ -1,11 +1,12 @@
-
 fn main() {
-
-    let numbers= vec![1, 2, 3, 4, 5];
-    let squares: Vec<i32> = numbers
-    .iter()
-    .map(|num| num * num)
-    .collect();
+    let numbers = vec![1, 2, 3, 4, 5];
+    let squares: Vec<i32> = numbers.iter().map(|num| num * num).collect();
     println!("{:?}", squares);
-    
+
+    let ages = vec![12, 45, 18, 7, 33, 16, 21];
+
+    // TODO: Use iterators to get only ages 18+
+    // Expected: [45, 18, 33, 21]
+    let adults: Vec<_>= ages.iter().filter(|&&age|  age >= 18).collect();
+    println!("{:?}", adults);
 }
