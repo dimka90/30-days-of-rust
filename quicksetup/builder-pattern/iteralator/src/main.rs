b1196a1a124b4731673e21147f664bb7a277f3a1
dimka90 <dimkayilrit@gmail.com>
2025-11-06 08:48:10 +0100

    implemented custom filter_map

--- quicksetup/builder-pattern/iteralator/src/main.rs
+++ quicksetup/builder-pattern/iteralator/src/main.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 fn main() {
     let numbers = vec![1, 2, 3, 4, 5];
     let squares: Vec<i32> = numbers.iter().map(|num| num * num).collect();
@@ -5,8 +7,24 @@ fn main() {
 
     let ages = vec![12, 45, 18, 7, 33, 16, 21];
 
-    // TODO: Use iterators to get only ages 18+
-    // Expected: [45, 18, 33, 21]
+    // Use iterators to get only ages 18+
     let adults: Vec<_>= ages.iter().filter(|&&age|  age >= 18).collect();
     println!("{:?}", adults);
+
+    let strings = vec!["42", "not a number", "100", "7", "oops"];
+
+    // Parse to i32, keep only successful parses
+    let nums: Vec<i32>= strings
+                         .iter()
+                         .filter_map(|&item| item.parse::<i32>().ok())
+                         .collect();
+
+    //My custom implementation
+    let nums: Vec<_>= strings
+               .iter()
+               .map(|&item| item.parse::<i32>().ok())
+               .filter(|item| item.is_some())
+               .map(|item| item.unwrap())
+               .collect();
+    println!("{:?}", nums);
 }
