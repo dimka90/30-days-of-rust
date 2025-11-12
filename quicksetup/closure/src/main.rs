e6bb6640430f39c7b698e7d540b1a86278492d39
dimka90 <dimkayilrit@gmail.com>
2025-11-12 18:57:01 +0100

    multiplier: implemented a closure that multiples numbers

--- quicksetup/closure/src/main.rs
+++ quicksetup/closure/src/main.rs
@@ -1,7 +1,10 @@
-
-
 fn main() {
-     let sum = |x: f64, y: f64| x+ y;
-    let result = sum(9.0,4.0);
+    let sum = |x: f64, y: f64| x + y;
+    let result = sum(9.0, 4.0);
     println!("{result}");
+
+    let multiplier = 5;
+    let multiple = |x: i32| x * multiplier;
+    let product = multiple(9);
+    println!("{product}");
 }
