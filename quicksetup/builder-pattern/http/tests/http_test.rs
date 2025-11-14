9d4608ae5b2a4708af62b126d2e7e5b6a443e7c9
dimka90 <dimkayilrit@gmail.com>
2025-11-14 07:39:31 +0100

    test: added test for http body request

--- quicksetup/builder-pattern/http/tests/http_test.rs
+++ quicksetup/builder-pattern/http/tests/http_test.rs
@@ -23,3 +23,31 @@ fn test_http_request_builder_method(){
     assert_eq!(build.method, http_method);
     assert_eq!(build.headers, Vec::new());
 }
+
+#[test]
+fn test_http_request_builder_url() {
+    let url = "www.dimka.com";
+    let http_method = HttpMethod::POST;
+    let build = HttpRequestBuilder::new(&url)
+                                    .method(HttpMethod::POST);
+
+    assert_eq!(build.url, url);
+    assert_eq!(build.body, None);
+    assert_eq!(build.method, http_method);
+    assert_eq!(build.headers, Vec::new());
+}
+
+#[test]
+fn test_http_request_builder_body() {
+    let url = "www.dimka.com";
+    let http_method = HttpMethod::POST;
+    let body = r#"{name:"dimka", "age": 20 }"#.to_string();
+    let build = HttpRequestBuilder::new(&url)
+                                    .method(HttpMethod::POST)
+                                    .body(body.clone());
+    assert_eq!(build.url, url);
+    assert_eq!(build.body, Some(body));
+    assert_eq!(build.method, http_method);
+    assert_eq!(build.headers, Vec::new());
+
+}
