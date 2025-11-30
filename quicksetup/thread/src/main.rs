1d1683c32a6a3ef53ebb49cfac5fc930dab293e2
dimka90 <dimkayilrit@gmail.com>
2025-11-30 16:55:51 +0100

    smart-pointer: Adds smart pointers

--- quicksetup/thread/src/main.rs
+++ quicksetup/thread/src/main.rs
@@ -1,17 +1,20 @@
-use std::{thread, time::Duration};
-
+use std::{ sync::Arc, thread, time::Duration};
 fn main() {
+
+    let  shared_value = Arc::new(5);
     println!("Hello, world!");
 
-    thread::spawn(|| {
-        for i in 1..10{
-            println!("Current Index {i}");
+    let handle = thread::spawn(move || {
+        for i in 1..=10 {
+            println!("Current Index {shared_value  }");
+            thread::sleep(Duration::from_millis(10));
         }
     });
 
     for i in 1..10{
         println!("From the main loop{i}");
-        thread::sleep(Duration::from_millis(30));
-
+        thread::sleep(Duration::from_millis(5));
     }
+   let _ =  handle.join();
 }
+
