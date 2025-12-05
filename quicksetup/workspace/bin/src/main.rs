5ac5d5eacc6ec8276b2d9adacd819da3e7332e6d
dimka90 <dimkayilrit@gmail.com>
2025-12-05 09:14:42 +0100

    Workspace: added both bin and executable together

--- quicksetup/workspace/bin/src/main.rs
+++ quicksetup/workspace/bin/src/main.rs
@@ -1,3 +1,8 @@
+use crates::{add, greet};
 fn main() {
+    let first_num = 4;
+    let second_num = 6;
+    let sum = first_num + second_num;
+    println!("Result:{sum}");
     println!("Hello, world!");
 }
